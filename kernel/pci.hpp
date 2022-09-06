951e2780f2b8b2ccb84fd43109f08861b652bc9c
Froslass Glalie <51141586+kakigori12345@users.noreply.github.com>
2022-09-06 09:50:40 +0900

    マウスカーソルを動かせるようにする（6-c）

--- kernel/pci.hpp
+++ kernel/pci.hpp
@@ -7,15 +7,30 @@
 
 
 namespace pci {
+  // PCI デバイスのクラスコード
+  struct ClassCode {
+    uint8_t base, sub, interface;
+
+    bool Match(uint8_t b) { return b == base; }
+    bool Match(uint8_t b, uint8_t s) { return Match(b) && s == sub; }
+    bool Match(uint8_t b, uint8_t s, uint8_t i) { return Match(b, s) && i == interface; }
+  };
+
+  struct Device {
+    uint8_t bus, device, function, header_type;
+    ClassCode class_code;
+  };
+
+  //-----------------------------
+  // PCI レジスタ操作用の関数群
+  //-----------------------------
+
   // ↓このアドレスは固定でこの値らしい
   // CONFIG_ADDRESS レジスタの IO ポートアドレス
   const uint16_t kConfigAddress = 0x0cf8;
   // CONFIG_DATA レジスタの IO ポートアドレス
   const uint16_t kConfigData = 0x0cfc;
 
-  //-----------------------------
-  // PCI レジスタ操作用の関数群
-  //-----------------------------
 
   void WriteAddress(uint32_t address);
   void WriteData(uint32_t value);
@@ -26,6 +41,10 @@ namespace pci {
   uint16_t ReadDeviceId(uint8_t bus, uint8_t device, uint8_t function);
   uint8_t ReadHeaderType(uint8_t bus, uint8_t device, uint8_t function);
 
+  inline uint16_t ReadVendorId(const Device& dev) {
+    return ReadVendorId(dev.bus, dev.device, dev.function);
+  }
+
   /** @brief クラスコードレジスタを読み取る（全ヘッダタイプ共通）
    *
    * 返される 32 ビット整数の構造は次の通り．
@@ -34,7 +53,7 @@ namespace pci {
    *   - 15:8  : インターフェース
    *   - 7:0   : リビジョン
    */
-  uint32_t ReadClassCode(uint8_t bus, uint8_t device, uint8_t function);
+  ClassCode ReadClassCode(uint8_t bus, uint8_t device, uint8_t function);
 
   /** @brief バス番号レジスタを読み取る（ヘッダタイプ 1 用）
    *
@@ -47,13 +66,20 @@ namespace pci {
 
   bool IsSingleFunctionDevice(uint8_t header_type);
 
-  struct Device {
-      uint8_t bus, device, function, header_type;
-  };
-
   // PCI デバイス登録用配列
   inline std::array<Device, 32> devices; //PCIデバイス一覧
   inline int num_device;
 
   Error ScanAllBus();
+
+  /** @brief 指定された PCI デバイスの 32 ビットレジスタを読み取る */
+  uint32_t ReadConfReg(const Device& dev, uint8_t reg_addr);
+
+  void WriteConfReg(const Device& dev, uint8_t reg_addr, uint32_t value);
+
+  constexpr uint8_t CalcBarAddress(unsigned int bar_index) {
+    return 0x10 + 4 * bar_index;
+  }
+
+  WithError<uint64_t> ReadBar(Device& device, unsigned int bar_index);
 }
