6280b631e66ecee45a0499509890ffb7652d38aa
Froslass Glalie <51141586+kakigori12345@users.noreply.github.com>
2022-09-18 13:11:09 +0900

    複数タイマとタイムアウト通知実装（11-d）

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -114,7 +114,9 @@ extern "C" void KernelMainNewStack(
 
   SetLayerUpDown();
 
-  InitializeLAPICTimer();
+  InitializeLAPICTimer(*main_queue);
+  timer_manager->AddTimer(Timer(200, 2));
+  timer_manager->AddTimer(Timer(600, -1));
 
   char str[128];
   // メッセージ処理ループ
@@ -142,8 +144,12 @@ extern "C" void KernelMainNewStack(
     case Message::kInterruptXHCI:
       usb::xhci::ProcessEvents();
       break;
-    case Message::kInterruptLAPICTimer:
-      printk("Timer interrput\n");
+    case Message::kTimerTimeout:
+      printk("Timer: timeout = %lu, value = %d\n",
+        msg.arg.timer.timeout, msg.arg.timer.value);
+      if(msg.arg.timer.value > 0){
+        timer_manager->AddTimer(Timer(msg.arg.timer.timeout + 100, msg.arg.timer.value + 1));
+      }
       break;
     default:
       Log(kError, "Unknown message type: %d\n", msg.type);
--- kernel/timer.hpp
+++ kernel/timer.hpp
@@ -1,20 +1,44 @@
 #pragma once
 
 #include <cstdint>
+#include <queue>
+#include <deque>
+#include <vector>
 
-void InitializeLAPICTimer();
+#include "message.hpp"
+
+void InitializeLAPICTimer(std::deque<Message>& msg_queue);
 void StartLAPICTimer();
 uint32_t LAPICTimerElapsed();
 void StopLAPICTimer();
-void LAPICTimerOnInterrupt();
+
+class Timer {
+  public:
+    Timer(unsigned long timeout, int value);
+    unsigned long Timeout() const {return timeout_;}
+    int Value() const {return value_;}
+
+  private:
+    unsigned long timeout_;
+    int value_;
+};
+
+inline bool operator<(const Timer& lhs, const Timer& rhs) {
+  return lhs.Timeout() > rhs.Timeout();
+}
 
 class TimerManager {
   public:
+    TimerManager(std::deque<Message>& msg_queue);
+    void AddTimer(const Timer& timer);
     void Tick();
     unsigned long CurrentTick() const {return tick_;}
     
   private:
     volatile unsigned long tick_{0}; //割り込み処理の中でしか更新されないので volatile 
+    std::priority_queue<Timer> timers_{};
+    std::deque<Message>& msg_queue_;
 };
 
 extern TimerManager* timer_manager;
+void LAPICTimerOnInterrupt();
