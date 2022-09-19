4867ab4c47855c99cb0f69978e2bbd5da7d02bb1
Froslass Glalie <51141586+kakigori12345@users.noreply.github.com>
2022-09-19 18:17:17 +0900

    点滅するカーソルを描画する（12-f）

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -73,6 +73,8 @@ void InitializeMainWindow(PixelFormat pixel_format){
 // テキストウィンドウ
 std::shared_ptr<Window> text_window;
 unsigned int text_window_layer_id;
+int text_window_index;
+
 void InitializeTextWindow() {
   const int win_w = 300;
   const int win_h = 200;
@@ -90,7 +92,12 @@ void InitializeTextWindow() {
     .ID();
 }
 
-int text_window_index = 0;
+void DrawTextCursor(bool visible){
+  const auto color = visible ? ToColor(0) : ToColor(0xffffff);
+  const auto pos = Vector2D<int>{8 + 8*text_window_index, 24 + 5};
+  FillRectangle(*text_window->Writer(), pos, {7,15}, color);
+}
+
 void InputTextWindow(char c) {
   if(c == 0){
     return;
@@ -98,18 +105,23 @@ void InputTextWindow(char c) {
 
   auto pos = []() { return Vector2D<int>{8 + 8 * text_window_index, 24 + 6}; };
 
-  const int max_chars = (text_window->Width() - 16) / 8;
+  const int max_chars = (text_window->Width() - 16) / 8 - 1;
   if(c == '\b' && text_window_index > 0){
+    DrawTextCursor(false); //前書いたカーソルを消す
     --text_window_index;
     FillRectangle(*text_window->Writer(), pos(), {8, 16}, ToColor(0xffffff));
+    DrawTextCursor(true);
   }
   else if(c >= ' ' && text_window_index < max_chars) {
+    DrawTextCursor(false);
     WriteAscii(*text_window->Writer(), pos(), c, ToColor(0));
     ++text_window_index;
+    DrawTextCursor(true);
   }
 
   layer_manager->Draw(text_window_layer_id);
 }
+
 
 
 void SetLayerUpDown() {
@@ -166,6 +178,15 @@ extern "C" void KernelMainNewStack(
 
   InitializeKeyboard(*main_queue);
 
+  // カーソル点滅用タイマの生成
+  const int kTextboxCursorTimer = 1;
+  const int kTimer05sec = static_cast<int>(kTimerFreq * 0.5);
+  __asm__("cli");
+  timer_manager->AddTimer(Timer{kTimer05sec, kTextboxCursorTimer, "ForCursor"});
+  __asm__("sti");
+  bool textbox_cursor_visible = false;
+
+
   char str[128];
   // メッセージ処理ループ
   while(true) {
@@ -193,8 +214,14 @@ extern "C" void KernelMainNewStack(
       usb::xhci::ProcessEvents();
       break;
     case Message::kTimerTimeout:
-      printk("Timer: timeout = %lu, value = %d, description = %s\n",
-        msg.arg.timer.timeout, msg.arg.timer.value, msg.arg.timer.description);
+      if(msg.arg.timer.value == kTextboxCursorTimer) {
+        __asm__("cli");
+        timer_manager->AddTimer(Timer{msg.arg.timer.timeout + kTimer05sec, kTextboxCursorTimer, msg.arg.timer.description});
+        __asm__("sti");
+        textbox_cursor_visible = !textbox_cursor_visible;
+        DrawTextCursor(textbox_cursor_visible);
+        layer_manager->Draw(text_window_layer_id);
+      }
       break;
     case Message::kKeyPush:
       InputTextWindow(msg.arg.keyboard.ascii);
