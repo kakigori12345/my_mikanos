53a0fbaf832594f0c9d0f3f8463df24ea3b3faf1
Kaigori123 <e1212@ob.akashi.ac.jp>
2022-10-16 19:34:41 +0900

    パイプ処理（29-b）

--- kernel/message.hpp
+++ kernel/message.hpp
@@ -22,6 +22,7 @@ struct Message {
     kMouseMove,
     kMouseButton,
     kWindowActive,
+    kPipe,
   } type;
 
   uint64_t src_task; //送信元タスクID
@@ -62,6 +63,11 @@ struct Message {
     struct {
       int activate; // 1: activate, 0: deactivate
     } window_active;
+
+    struct {
+      char data[16];
+      uint8_t len;
+    } pipe;
 
   } arg;
 };
--- kernel/task.hpp
+++ kernel/task.hpp
@@ -14,6 +14,7 @@
 #include <vector>
 #include <memory>
 #include <deque>
+#include <map>
 #include <optional>
 
 
@@ -109,7 +110,8 @@ class TaskManager {
   public:
     Task& CurrentTask();
     Error SendMessage(uint64_t id, const Message& msg);
-  
+    void Finish(int exit_code);
+    WithError<int> WaitFinish(uint64_t task_id);
 
   private:
     void _ChangeLevelRunning(Task* task, int level);
@@ -121,6 +123,8 @@ class TaskManager {
     std::array<std::deque<Task*>, kMaxLevel + 1> running_{};
     int current_level_{kMaxLevel};
     bool is_level_changed_{false};
+    std::map<uint64_t, int> finish_tasks_{}; //key: ID of a finished task
+    std::map<uint64_t, Task*> finish_waiter_{}; //key: ID of a finished task
 };
 
 extern TaskManager* task_manager;
